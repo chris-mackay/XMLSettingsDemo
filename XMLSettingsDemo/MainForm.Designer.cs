5219c3073a5cd2c5baa3ecd29fe60a65a05c97ed
Christopher Mackay <cmackay732@gmail.com>
2021-05-22 19:25:02 -0400

    Added files

---
+++ XMLSettingsDemo/MainForm.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace XMLSettingsDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rbNone = new System.Windows.Forms.RadioButton();
+            this.rbBorderSingle = new System.Windows.Forms.RadioButton();
+            this.rbBorder3D = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(413, 412);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.TabStop = false;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(494, 412);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.TabStop = false;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // div
+            // 
+            this.div.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.div.Location = new System.Drawing.Point(12, 395);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(557, 1);
+            this.div.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(292, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rbNone
+            // 
+            this.rbNone.AutoSize = true;
+            this.rbNone.Location = new System.Drawing.Point(16, 21);
+            this.rbNone.Name = "rbNone";
+            this.rbNone.Size = new System.Drawing.Size(51, 17);
+            this.rbNone.TabIndex = 0;
+            this.rbNone.Text = "None";
+            this.rbNone.UseVisualStyleBackColor = true;
+            this.rbNone.CheckedChanged += new System.EventHandler(this.rbNone_CheckedChanged);
+            // 
+            // rbBorderSingle
+            // 
+            this.rbBorderSingle.AutoSize = true;
+            this.rbBorderSingle.Location = new System.Drawing.Point(16, 44);
+            this.rbBorderSingle.Name = "rbBorderSingle";
+            this.rbBorderSingle.Size = new System.Drawing.Size(82, 17);
+            this.rbBorderSingle.TabIndex = 1;
+            this.rbBorderSingle.Text = "Fixed Single";
+            this.rbBorderSingle.UseVisualStyleBackColor = true;
+            this.rbBorderSingle.CheckedChanged += new System.EventHandler(this.rbFixedSingle_CheckedChanged);
+            // 
+            // rbBorder3D
+            // 
+            this.rbBorder3D.AutoSize = true;
+            this.rbBorder3D.Location = new System.Drawing.Point(16, 67);
+            this.rbBorder3D.Name = "rbBorder3D";
+            this.rbBorder3D.Size = new System.Drawing.Size(67, 17);
+            this.rbBorder3D.TabIndex = 2;
+            this.rbBorder3D.Text = "Fixed 3D";
+            this.rbBorder3D.UseVisualStyleBackColor = true;
+            this.rbBorder3D.CheckedChanged += new System.EventHandler(this.rbFixed3D_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(32, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Choose the preferred default border style for the picture box";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(32, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Default border style";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(32, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Background color";
+            // 
+            // label4
+            // 
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(32, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(221, 34);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Choose the preferred default background color for the picture box";
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(43, 324);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(186, 23);
+            this.btnColor.TabIndex = 6;
+            this.btnColor.TabStop = false;
+            this.btnColor.Text = "Choose background color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbBorderSingle);
+            this.groupBox1.Controls.Add(this.rbNone);
+            this.groupBox1.Controls.Add(this.rbBorder3D);
+            this.groupBox1.Location = new System.Drawing.Point(43, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(186, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtColor
+            // 
+            this.txtColor.Enabled = false;
+            this.txtColor.Location = new System.Drawing.Point(43, 298);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.ReadOnly = true;
+            this.txtColor.Size = new System.Drawing.Size(186, 20);
+            this.txtColor.TabIndex = 5;
+            this.txtColor.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 447);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XML Settings";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label div;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton rbNone;
+        private System.Windows.Forms.RadioButton rbBorderSingle;
+        private System.Windows.Forms.RadioButton rbBorder3D;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtColor;
+    }
+}
+
---
+++ XMLSettingsDemo/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+
+namespace XMLSettingsDemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            StringCollection settings = new StringCollection();
+
+            settings.Add("BorderNone,True");
+            settings.Add("BorderSingle,False");
+            settings.Add("Border3D,False");
+            settings.Add("PictureBoxBackColor,#F0F0F0");
+
+            XMLSettings.AppSettingsFile = "Settings.xml";
+            XMLSettings.InitializeSettings(settings);
+
+            rbNone.Checked = bool.Parse(XMLSettings.GetSettingsValue("BorderNone"));
+            rbBorderSingle.Checked = bool.Parse(XMLSettings.GetSettingsValue("BorderSingle"));
+            rbBorder3D.Checked = bool.Parse(XMLSettings.GetSettingsValue("Border3D"));
+            txtColor.Text = XMLSettings.GetSettingsValue("PictureBoxBackColor");
+            pictureBox1.BackColor = ColorTranslator.FromHtml(txtColor.Text);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XMLSettings.SetSettingsValue("BorderSingle", rbNone.Checked.ToString());
+                XMLSettings.SetSettingsValue("BorderSingle", rbBorderSingle.Checked.ToString());
+                XMLSettings.SetSettingsValue("Border3D", rbBorder3D.Checked.ToString());
+                XMLSettings.SetSettingsValue("PictureBoxBackColor", txtColor.Text);
+
+                MessageBox.Show("Settings have been saved", "XML Settings", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "XML Settings", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void rbNone_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.None;
+        }
+
+        private void rbFixedSingle_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void rbFixed3D_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            cd.FullOpen = true;
+
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = cd.Color;
+                txtColor.Text = ColorTranslator.ToHtml(cd.Color);
+            }
+        }
+    }
+}
